94ca7e5a4de6d8734a8f7be892122d322bd0b091
Erik Anderson <eanderson443@gmail.com>
2021-02-20 12:42:42 -0500

    init

---
+++ readSensors.ino
@@ -0,0 +1,18 @@
+int sensorPin = A5;
+int sensorPin2 = A4;
+int sensorValue = 0;
+int sensorValue2 = 0;
+void setup() {
+  Serial.begin(1200);
+
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  sensorValue2 = analogRead(sensorPin2)/2;
+  Serial.print(sensorValue);
+  Serial.print(",");
+  Serial.println(sensorValue2);
+  delay(200);
+
+}
